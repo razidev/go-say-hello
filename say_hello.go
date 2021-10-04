dd9e7b1034f562eacc58835db02c2deddfce7501
razidev <razi.syahputro@modalrakyat.id>
2021-10-04 18:31:29 +0700

    update return

--- say_hello.go
+++ say_hello.go
@@ -1,7 +1,7 @@
 package gosayhello
 
 func SayHello(name string) string {
-	return "Hello " + name
+	return "Hello developer handal" + name
 }
 
 //untuk melakukan perubahan versi setelah add - push lakukan
